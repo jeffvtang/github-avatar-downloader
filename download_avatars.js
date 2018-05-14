baaab27cf89086c50acc75795f7f7689095da952
Jeff <jeff.tang@live.com>
2018-05-14 18:20:43 -0400

    partially working, downloading all images as jpg

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var secrets = require('./secrets')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -14,17 +15,50 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   request(options, function (err, res, body) {
     if (!err) {
+      cb(err, body)
       var data = JSON.parse(body)
       data.forEach(function (user) {
-        console.log(user.avatar_url)
+        // console.log(user.avatar_url)
+        downloadImageByURL(user.avatar_url, './images/' + user.login + '.jpg')
       })
       // console.log(data)
-      cb(err, body);
     }
   });
+}
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode, 'Response Message', response.statusMessage, 'Content Type', response.headers['content-type']);
+      // console.log('Downloading image...');
+    })
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', function () {
+      console.log('Download complete.');
+    })
+  // ...
 }
 
 getRepoContributors("jquery", "jquery", function (err, result) {
   console.log("Errors:", err);
   // console.log("Result:", result);
 });
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg")
+
+
+// request.get('https://sytantris.github.io/http-examples/future.jpg')
+//   .on('error', function (err) {
+//     throw err;
+//   })
+//   .on('response', function (response) {
+//     console.log('Response Status Code: ', response.statusCode, 'Response Message', response.statusMessage, 'Content Type', response.headers['content-type']);
+//     console.log('Downloading image...')
+//   })
+//   .pipe(fs.createWriteStream('./future.jpg'))
+//   .on('finish', function () {
+//     console.log('Download complete.');
+//   })
