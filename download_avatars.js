a46d4340cb53bc02cbe3a6bd853298691830ad13
Jeff <jeff.tang@live.com>
2018-05-14 19:50:41 -0400

    working as is, need to clean up code and include better naming schemes

--- download_avatars.js
+++ download_avatars.js
@@ -2,9 +2,10 @@ const request = require('request');
 const fs = require('fs');
 const secrets = require('./secrets');
 let repoOwner = process.argv[2];
+// First user input
 let repoName = process.argv[3];
+// Second user input
 
-console.log('Welcome to the GitHub Avatar Downloader!');
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
@@ -21,7 +22,7 @@ function downloadImageByURL(url, filePath) {
   // });
 }
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, AvatarIteratorHandler) {
   const options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
@@ -32,23 +33,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   request(options, function (err, res, body) {
     if (!err) {
-      cb(err, body);
-      const data = JSON.parse(body);
-      data.forEach(function (user) {
-        // console.log(user.avatar_url)
-        downloadImageByURL(user.avatar_url, './avatars/' + user.login);
-      });
+      AvatarIteratorHandler(err, body);
       // console.log(data)
     }
   });
 }
 
+// Greeting message when running program
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+// If given an empty parameter, returns an error
 if (repoOwner == null || repoName == null) {
   console.log('Error, please confirm Repo Name and Repo Owner are correct')
 } else {
-  getRepoContributors(repoOwner, repoOwner, function (err, res) {
-    // console.log('Errors:', err);
-    // console.log("Result:", res.length, ' avatars downloaded');
-    console.log('Avatars Downloded')
+  getRepoContributors(repoOwner, repoOwner, function (error, result) {
+    // Parses the data from getRepoContributors function into an object
+    const data = JSON.parse(result);
+    // forEach loop that passes the URL for each avatar, and the destination file path to downloagImageByURL function
+    data.forEach(function (user) {
+      // console.log(user.avatar_url)
+      downloadImageByURL(user.avatar_url, './avatars/' + user.login);
+    });
+    // Logs the number of avatars downloaded
+    console.log(data.length + ' avatars downloaded')
   })
 }
