0c814294a5f480c41a10c69ab3506d8c91a09ab2
Jeff <jeff.tang@live.com>
2018-05-14 17:14:59 -0400

    added authorization token

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var secrets = require(./secrets)
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -6,16 +7,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
     }
   };
 
-  request(options, function(err, res, body) {
+  request(options, function (err, res, body) {
     cb(err, body);
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function (err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+//curl -i -H 'Authorization: token fb869e782a7a5d39b1c4d1f180e68cbab30e14ea' https://api.github.com/repos/jquery/jquery/contributors
