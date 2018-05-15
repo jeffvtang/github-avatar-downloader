aa9f882194812726f5b19858c0f5cb24487d288a
Jeff <jeff.tang@live.com>
2018-05-15 00:01:44 -0400

    added comments in code

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,11 @@
 const request = require('request');
 const fs = require('fs');
 const access = require('./secrets');
+// accepts user input from node
 let repoOwner = process.argv[2];
 let repoName = process.argv[3];
 
-
+// downloads images from a given URL to a specified file path
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', function (accessError) {
@@ -14,6 +15,7 @@ function downloadImageByURL(url, filePath) {
     .pipe(fs.createWriteStream(filePath));
 }
 
+// grabs the Repo Contributor data and
 function getRepoContributors(repoOwner, repoName, AvatarIteratorHandler) {
   const httpOptions = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
@@ -28,14 +30,18 @@ function getRepoContributors(repoOwner, repoName, AvatarIteratorHandler) {
   });
 }
 
+// greeting messaged upon running program
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// if inputs are valid, runs function to get Repo Contributor data and then downloads images as a callback function
 if (repoOwner == null || repoName == null) {
   console.log('Error, please confirm Repo Name and Repo Owner are correct')
 } else {
   getRepoContributors(repoOwner, repoName, function (error, contributorsData, httpOptions) {
     const repoContributors = JSON.parse(contributorsData);
+    // if the parsed data is the expected type continues the downloading process
     if (Array.isArray(repoContributors) && !error) {
+      // creates the directory for the images if it does not exist yet
       if (!fs.existsSync('./avatars')) {
         fs.mkdirSync('./avatars');
       }
